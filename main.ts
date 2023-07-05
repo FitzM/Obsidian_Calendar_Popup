41494478864f1d65cef6efaca3a09d2e93137c00
FitzM <giffted17@gmail.com>
2023-07-05 08:37:20 -0600

    Implemented matching the event-Id with what's clicked on the calendar and the assoicated note.

--- main.ts
+++ main.ts
@@ -1,31 +1,68 @@
-import {Plugin, Modal, WorkspaceLeaf, App, WorkspaceSidedock, WorkspaceSplit, TFile, ItemView, WorkspaceRibbon} from 'obsidian';
+import {Plugin, Modal, WorkspaceLeaf, App, WorkspaceSidedock, WorkspaceSplit, TFile, ItemView, WorkspaceRibbon, parseFrontMatterEntry, parseFrontMatterStringArray, parseFrontMatterTags} from 'obsidian';
 import * as React from 'react';
 import { createRoot } from "react-dom/client";
 import * as ReactDom from 'react-dom';
 import { render, unmountComponentAtNode } from 'react-dom';
-import CardsBox from './components/CardsBox';
+import { match } from 'assert';
+import { parse } from 'path';
+import { stringify } from 'querystring';
+import {CardsBox} from './components/CardsBox'
 
       // Test String Array
-let TestTodoList = ["Change the Laundry","Go to the store","Get Gas","Feed the Dogs","Give Courtney Massage"];
+let todoTasksToDisplay:string [] = [];
+
+
 
 //Load plugin and apply eventListener for opening view
-export default class TimeblockingEvent extends Plugin {
+export default class TimeblockingEvent  extends Plugin {
     async onload() {
      
       this.registerView("my-custom-view", leaf => new MyCustomView(leaf))
-    
+      
+      let filePath!: string;
       var calEvents = document.querySelectorAll<HTMLDivElement>('.fc-event-main');
+     let googleEvents = document.querySelectorAll < HTMLDivElement > ('.googleCalendarEvent');
+     googleEvents.forEach((calEvent) => {
+         calEvent.addEventListener('click', event => {
+             calEvent.style.backgroundColor = 'blue';
+             const clickEventId = (event.currentTarget as HTMLDivElement).id;
+             const allFiles = app.vault.getFiles();
+             console.log(clickEventId)
+             const matchingFiles = allFiles.filter((file) => {
+              const frontmatter = app.metadataCache.getFileCache(file)?.frontmatter;
+              const eventId = parseFrontMatterEntry(frontmatter, "event-id");
+              const todoListValue = parseFrontMatterEntry(frontmatter, "todo-list");
+              if (eventId == clickEventId) {
+                console.log("SUCCESS!!");
+                console.log(todoListValue);
+              filePath = "TodoSublists/"+todoListValue+".md";
+              console.log(filePath)
+                return true; // Include the file in the filtered results
+              }
+              //console.log("negative")
+              return false; // Exclude the file from the filtered results
+            });
+            console.log(filePath)
+             readFileAndSplit(filePath).then((todoTasksToDisplay) => {
+              
+              console.log(todoTasksToDisplay)
+             });
 
-      calEvents.forEach((calEvent) => {
-       calEvent.addEventListener('click', () => {
-         calEvent.style.backgroundColor = 'blue';
-        const leaf = this.app.workspace.getRightLeaf(false);
-        this.app.workspace.revealLeaf(leaf);
-        leaf.setViewState({ type: "my-custom-view", active: true });  
-         console.log('Did the Color change?!');
-       });
-     }); 
-    }}
+          
+   
+    
+            const leaf = this.app.workspace.getRightLeaf(false);
+           this.app.workspace.revealLeaf(leaf);
+            leaf.setViewState({ type: "my-custom-view", active: true });  
+            console.log(todoTasksToDisplay);
+            console.log('Did the Color change?!');
+          
+          })
+        })
+       
+          }}
+          
+  
 
 export class ExampleModal extends Modal {
   constructor(app: App) {
@@ -46,8 +83,8 @@ export class ExampleModal extends Modal {
 
     export const VIEW_TYPE_EXAMPLE = "My-custom-view";
     export class MyCustomView extends ItemView {
-      constructor(leaf: WorkspaceLeaf) {
-        
+     public constructor(
+        leaf: WorkspaceLeaf) {
         super(leaf);
       }
     
@@ -61,7 +98,8 @@ export class ExampleModal extends Modal {
     
       async onOpen() {
         const root = createRoot(this.containerEl.children[1]);
-        root.render(CardsBox(TestTodoList));
+        console.log(todoTasksToDisplay);
+        root.render((<CardsBox{todoTasksToDisplay}/>));
       }
 
       async activateView() {
@@ -77,4 +115,30 @@ export class ExampleModal extends Modal {
     }
 
 
+    function getListOfSubtasks(){
+
+
+      if(app.metadataCache == null){
+        return}
+      
+     // const noteFrontMatter = app.metadataCache.getFileCache(TFile)?.frontmatter
+    }
+
+    async function readFileAndSplit(filePath: string) {
+      let todoFile = await this.app.vault.getAbstractFileByPath(filePath);
+      console.log(todoFile);
+    
+      if (todoFile instanceof TFile) {
+        let noteContent = await this.app.vault.read(todoFile);
+        const todoTasksToDisplay: string[] = noteContent.split('\n');
+    
+        // Log the parsed items
+        todoTasksToDisplay.forEach((item) => {
+          console.log(item);
+        });
+        return todoTasksToDisplay;
+      }
+    }
+    
+    
     
